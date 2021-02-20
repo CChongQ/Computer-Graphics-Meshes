b73144a45f621485c9644c307914faaf5b406ac3
Claire Qiu <claireqch@gmail.com>
2021-02-19 22:54:21 -0500

    init code - first 2 complete

--- src/write_obj.cpp
+++ src/write_obj.cpp
@@ -15,6 +15,41 @@ bool write_obj(
   assert((F.size() == 0 || F.cols() == 3 || F.cols() == 4) && "F must have 3 or 4 columns");
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here:
+  
+  std::ofstream obj_file;
+  obj_file.open(filename);
+
+  //List of geometric vertices
+  for (int i = 0; i< V.rows(); i++){
+    std::string line = "v " + V(i,0) + " " + V(i,1) + " " + V(i,2) + "\n";
+    obj_file << line;
+  }
+
+  //List of texture coordinates
+  for (int i = 0; i< UV.rows(); i++){
+    std::string line = "vt " + UV(i,0) + " " + UV(i,1)  + "\n";
+    obj_file << line;
+  }
+
+  //List of vertex normals
+  for (int i = 0; i< NV.rows(); i++){
+    std::string line = "vn " + NV(i,0) + " " + NV(i,1) + " " + NV(i,2) + "\n";
+    obj_file << line;
+  }
+
+  //Polygonal face element
+  //f v1/vt1/vn1 v2/vt2/vn2 v3/vt3/vn3
+  for (int i = 0; i< F.rows(); i++){
+    std::string line = "f";
+    for (int j  0; j< F.cols(); j++){
+      line = line + " " + (F(i,j)+1) + "/" + (UV(i,j)+1) + "/" + (NV(i,j)+1);
+    }
+    line +="\n";
+    obj_file << line;
+  }
+  
+  obj_file.close();
+
   ////////////////////////////////////////////////////////////////////////////
   return false;
 }
